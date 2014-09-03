0f1c15eb0d2908bb5251eca4b33be1ad86c92f1a
Gam- <1570601@lfschools.net>
2014-09-03 08:33:39 -0500

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('Developers! Developers! Developers!')
+  response.send('Developers! BAllmer! Developers!')
 })
 
 app.listen(app.get('port'), function() {
