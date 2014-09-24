cc50e7baa1da4dbe0aac3e449622dbac0a186ae4
Gam- <1570601@lfschools.net>
2014-09-24 14:57:06 +0000

    changed shit

--- web.js
+++ web.js
@@ -1,14 +1,14 @@
 var express = require('express');
 var fs = require('fs');
 
-var data = fs.readFileSync("index.html", "utf-8");
-
-
 var app = express.createServer(express.logger());
+
 app.get('/', function(request, response) {
-  response.send(data);
+  var content = fs.readFileSync('index.html', 'utf8');
+  response.send(content);
 });
-var port = process.env.PORT || 5000;
+
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
